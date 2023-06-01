8e80f41bcdbbcfdfed6371fdd58345bb88fb3bba
Patka879 <mysiak.patrycja@gmail.com>
2023-06-01 22:34:45 +0200

    Reservation entity created

--- src/main/java/com/example/finalProject/model/Reservation.java
+++ src/main/java/com/example/finalProject/model/Reservation.java
@@ -1,12 +1,111 @@
 package com.example.finalProject.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
-@Table(name="organization")
+@Table(name = "reservation")
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(columnDefinition = "serial")
     private long id;
+
+    @NotBlank(message = "Reservation identifier is required")
+    @Size(min = 2, max = 20)
+    @Column(unique = true)
+    private String identifier;
+
+    @NotNull(message = "Organization is required")
+    @ManyToOne
+    @JoinColumn(name = "organization_id", referencedColumnName = "id")
+    private Organization organization;
+
+    @NotNull(message = "Room is required")
+    @ManyToOne
+    @JoinColumn(name = "room_id", referencedColumnName = "id")
+    private Room room;
+
+    @NotNull(message = "Date is required")
+    private LocalDate date;
+
+    @NotNull(message = "Start time is required")
+    private LocalTime startTime;
+
+    @NotNull(message = "End time is required")
+    private LocalTime endTime;
+
+    public Reservation() {
+    }
+
+    public Reservation(String identifier, Organization organization, Room room, LocalDate date,
+                       LocalTime startTime, LocalTime endTime) {
+        this.identifier = identifier;
+        this.organization = organization;
+        this.room = room;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
 }
